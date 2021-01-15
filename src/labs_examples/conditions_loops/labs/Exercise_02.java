4f6f577020def1a4a50b1a5ccbc824443625b371
Devarat Patel <Devaratpatel1@gmail.com>
2021-01-14 19:20:58 -0500

    cont. conditon loops

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,45 +1,49 @@
 package labs_examples.conditions_loops.labs;
+
 import java.util.*;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
- *
- *      Take in a number from the user and print "Monday", "Tuesday", ... "Sunday", or "Other"
- *      if the number from the user is 1, 2,... 7, or other respectively. Use an if-else statement
- *      to accomplish this task.
- * 
- *      Bonus Tricky Challenge: Use a "nested-if" statement.
- *
+ * <p>
+ * Take in a number from the user and print "Monday", "Tuesday", ... "Sunday", or "Other"
+ * if the number from the user is 1, 2,... 7, or other respectively. Use an if-else statement
+ * to accomplish this task.
+ * <p>
+ * Bonus Tricky Challenge: Use a "nested-if" statement.
  */
 
 public class Exercise_02 {
 
     public static void main(String[] args) {
-
+        // 1) create scanner (don't forget to import Scanner!)
         Scanner sc = new Scanner(System.in);
+        // 2) prompt user
         System.out.println("Enter any number between 1-7");
+        // 3) assign input to variable as int
         int numDay = sc.nextInt();
+        // 4) write completed code here
 
-        if(numDay ==1 ) {
+        if (numDay == 1) {
             System.out.println("Sunday");
         }
-        if(numDay ==2 ) {
+        if (numDay == 2) {
             System.out.println("Monday");
         }
-        if(numDay ==3 ) {
+        if (numDay == 3) {
             System.out.println("Tuesday");
         }
-        if(numDay ==4 ) {
+        if (numDay == 4) {
             System.out.println("Wednesday");
         }
-        if(numDay ==5 ) {
+        if (numDay == 5) {
             System.out.println("Thursday");
         }
-
-
-        // 1) create scanner (don't forget to import Scanner!)
-        // 2) prompt user
-        // 3) assign input to variable as int
-        // 4) write completed code here
+        if (numDay == 6) {
+            System.out.println("Friday");
+        }
+        if (numDay == 7) {
+            System.out.println("Saturday");
+        }
 
     }
 }
