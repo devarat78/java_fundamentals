a32878e0e0034672134c066b2b7f21b8aed19df4
Devarat Patel <devaratpatel@Devarats-Air.home>
2021-02-20 12:37:15 -0500

    excercise 06

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -35,9 +35,6 @@ public class Exercise_06 {
         double surfaceArea = (2 * Math.PI * radius * height) + (2 * Math.PI* Math.pow(3.14,2));
         System.out.println("the surface area is: "+ surfaceArea);
 
-        for (int i= 0; i<=1000000; i++) {
-            System.out.println(i);
         }
 
     }
-}
